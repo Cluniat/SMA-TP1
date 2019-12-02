5d65a77c058e8cfc85e84c411179efc2ed0d6b61
Anthony Baccuet <anba97@gmail.com>
2019-12-02 17:11:09 +0100

    Agent et Grid

--- src/Agent.java
+++ src/Agent.java
@@ -1,2 +1,18 @@
-public class Agent {
+
+
+public class Agent extends{
+    private int [] currentPos;
+    private int [] goalPos;
+    static Agent [][] grid;
+    static List<Agent> agents;
+
+    public Agent(int[] currentPos, int[] goalPos) {
+        this.currentPos = currentPos;
+        this.goalPos = goalPos;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Salut");
+    }
+
 }
